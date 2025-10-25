c0b2b827d13fa5a173d052e116640274626d03a5
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-10-25 01:34:57 -0300

    Praticando e estudando
    Praticando conceitos teóricos de loops e condicionais.

--- projetos/aula14ex/ex017/script.js
+++ projetos/aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá, mundo!');
+
+const numero = document.getElementById('num')
+const tabuada = document.getElementById('tab')
+
+function gerarTabuada() {
+
+    if (numero.value.length == 0) {
+        numero.value = 0
+    }
+
+    const n = Number(numero.value)
+    tabuada.innerHTML = ''
+
+    for (let c = 1; c <= 10; c++) {
+        const item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n * c}`
+        item.value = `tab${c}`
+        tabuada.appendChild(item)
+    }
+}
