3e737db2c18f6a39122643bcb3a508f1d00e92e1
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-10-25 14:39:04 -0300

    Funções
    Estudo teórico de funções em JavaScript.

---
+++ projetos/aula15/ambiente.js
@@ -0,0 +1,14 @@
+
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(7)
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(3) // Valor solicitado: 3
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor solicitado está na posição ${pos}`)
+}
---
+++ projetos/aula16/funcao02.js
@@ -0,0 +1,8 @@
+
+function soma(a=0, b=0) {
+    return a + b;
+}
+
+console.log(soma(3, 5));
+
+// Se algum dos dois parâmetros não tiver valor, altomaticamente ele será 0 (zero).
